466c69f4f98675ab57ff592896f51b40e24b05c2
KaJuan Jones <47114033+kajuanjones@users.noreply.github.com>
2019-09-20 19:56:46 -0400

    Projectile and Proj Script
    Added Projectile game object with its Script. Wrote code that allows Hero to shoot.

--- Assets/Scripts/Hero.cs
+++ Assets/Scripts/Hero.cs
@@ -10,6 +10,9 @@ public class Hero : MonoBehaviour
     public float speed = 30;
     public float rollMult = -45;
     public float pitchMult = 30;
+    public float gameRestartDelay = 2f;
+    public GameObject projectilePrefab;
+    public float projectileSpeed = 40;
 
     [Header("Set Dyanmically")]
     public float _shieldLevel = 1;
@@ -36,6 +39,20 @@ public class Hero : MonoBehaviour
         pos.y += yAxis * speed * Time.deltaTime;
         transform.position = pos;
         transform.rotation = Quaternion.Euler(yAxis * pitchMult, xAxis * rollMult, 0);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            TempFire();
+        }
+    }
+
+    void TempFire()
+    {
+        GameObject projGO = Instantiate<GameObject>(projectilePrefab);
+        projGO.transform.position = transform.position;
+        Rigidbody rigidB = projGO.GetComponent<Rigidbody>();
+        rigidB.velocity = Vector3.up * projectileSpeed;
+
     }
 
     private void OnTriggerEnter(Collider other)
@@ -74,6 +91,7 @@ public class Hero : MonoBehaviour
             if (value < 0)
             {
                 Destroy(this.gameObject);
+                Main.S.DelayedRestart(gameRestartDelay);
             }
         }
     }
